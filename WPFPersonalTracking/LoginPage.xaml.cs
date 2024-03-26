3ac31d771182aa2006e9d642d8e00af49d5564ba
onurkiris <83399747+onurkiris05@users.noreply.github.com>
2024-03-26 15:52:56 +0300

    [Added] Login Page

---
+++ WPFPersonalTracking/LoginPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPFPersonalTracking.DB;
+
+namespace WPFPersonalTracking
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+        PersonaltrackingContext _db = new();
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        #region EVENT METHODS
+        private void txtUserNo_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (!AreFieldsValid()) return;
+
+            var employee = _db.Employees.FirstOrDefault(x => x.UserNo == Convert.ToInt32(txtUserNo.Text) &&
+            x.Password.Equals(txtPassword.Text));
+
+            if (employee != null && employee.Id != 0)
+            {
+                Visibility = Visibility.Collapsed;
+                var main = new MainWindow();
+                UserStatic.EmployeeId = employee.Id;
+                UserStatic.IsAdmin = employee.IsAdmin;
+                UserStatic.Name = employee.Name;
+                UserStatic.Surname = employee.Surname;
+                UserStatic.UserNo = employee.UserNo;
+                main.ShowDialog();
+            }
+            else
+                MessageBox.Show("Please make sure your password and user no are correct!");
+
+        }
+        #endregion
+
+        #region SIDE METHODS
+        private bool AreFieldsValid()
+        {
+            if (txtUserNo.Text.Trim() == "" || txtPassword.Text.Trim() == "")
+            {
+                MessageBox.Show("Please fill the necessary fields!");
+                return false;
+            }
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ WPFPersonalTracking/UserStatic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFPersonalTracking
+{
+    public static class UserStatic
+    {
+        public static int EmployeeId { get; set; }
+        public static bool? IsAdmin { get; set; }
+        public static int UserNo { get; set; }
+        public static string Name { get; set; }
+        public static string Surname { get; set; }
+    }
+}
